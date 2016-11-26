a019ba292368337ad63b109a35848d8d26210c23
khangx <sasuke.xiong@gmail.com>
2016-11-25 19:44:44 -0800

    screenshot

--- conf.js
+++ conf.js
@@ -59,9 +59,15 @@ exports.config = {
     // jasmine.getEnv().addReporter(new SpecReporter({displayStacktrace: 'all'}));
     // jasmine.getEnv().addReporter(reporter);
     var AllureReporter = require('./node_modules/jasmine-allure-reporter');
-    jasmine.getEnv().addReporter(new AllureReporter({
-      resultsDir: './allure-results'
-    }));
+    jasmine.getEnv().addReporter(new AllureReporter());
+    jasmine.getEnv().afterEach(function(done){
+      browser.takeScreenshot().then(function (png) {
+        allure.createAttachment('Screenshot', function () {
+          return new Buffer(png, 'base64')
+        }, 'image/png')();
+        done();
+      })
+    });
   },
 
   afterLaunch: function(exitCode) {
